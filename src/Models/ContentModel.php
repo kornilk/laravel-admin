e25e0c3180b4d478cb872e23e6d0f3d82f24e33e
Zsolt Körmöndi <zsolt@super11.hu>
2021-11-01 17:18:01 +0100

    Update

--- src/Models/ContentModel.php
+++ src/Models/ContentModel.php
@@ -4,8 +4,9 @@ namespace Encore\Admin\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Altek\Accountant\Contracts\Recordable;
 
-class ContentModel extends Model implements Auditable
+class ContentModel extends Model implements Recordable
 {
     use SoftDeletes;
     use \Altek\Eventually\Eventually;
