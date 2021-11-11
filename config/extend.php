f580b58d0ee730f6ff5ef3a2872e8c7229b899b3
Zsolt Körmöndi <zsolt@super11.hu>
2021-11-11 22:54:28 +0100

    Update

--- config/extend.php
+++ config/extend.php
@@ -1,14 +1,12 @@
 <?php
 
 return [
- 
+
     'domain' => env('PROJECT_DOMAIN', 'localhost'),
     'subdomain_api' => env('SUBDOMAIN_API', 'api'),
     'subdomain_admin' => env('SUBDOMAIN_ADMIN', 'admin'),
 
     'cache_expire_base' => env('CACHE_EXPIRE_BASE', 10),
     'cache_expire_translation' => env('CACHE_EXPIRE_TRANSLATION', 10),
-
-    'project_name' => env('PROJECT_NAME', ''),
-    
+        
 ];
