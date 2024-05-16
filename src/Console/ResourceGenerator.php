4a6d02eb333e9566fb9d9f84337fa2a6bdc5ae3c
Zsolt Körmöndi <zsolt@super11.hu>
2024-05-16 12:22:22 +0200

    Update to laravel 11

--- src/Console/ResourceGenerator.php
+++ src/Console/ResourceGenerator.php
@@ -83,12 +83,12 @@ class ResourceGenerator
         $output = '';
 
         foreach ($this->getTableColumns() as $column) {
-            $name = $column->getName();
+            $name = $column['name'];
 
             if (in_array($name, $reservedColumns)) continue;
 
-            $type = $column->getType()->getName();
-            $default = $column->getDefault();
+            $type = $column['type'];
+            $default = $column['default'];
 
             $defaultValue = '';
 
@@ -168,7 +168,7 @@ class ResourceGenerator
         $output = '';
 
         foreach ($this->getTableColumns() as $column) {
-            $name = $column->getName();
+            $name = $column['name'];
 
             if (in_array($name, $reservedColumns)) continue;
 
@@ -187,7 +187,7 @@ class ResourceGenerator
         $output = '';
 
         foreach ($this->getTableColumns() as $column) {
-            $name = $column->getName();
+            $name = $column['name'];
 
             if (in_array($name, $reservedColumns)) continue;
 
@@ -217,31 +217,9 @@ class ResourceGenerator
      */
     protected function getTableColumns()
     {
-        if (!$this->model->getConnection()->isDoctrineAvailable()) {
-            throw new \Exception(
-                'You need to require doctrine/dbal: ~2.3 in your own composer.json to get database columns. '
-            );
-        }
-
         $table = $this->model->getConnection()->getTablePrefix().$this->model->getTable();
-        /** @var \Doctrine\DBAL\Schema\MySqlSchemaManager $schema */
-        $schema = $this->model->getConnection()->getDoctrineSchemaManager($table);
 
-        // custom mapping the types that doctrine/dbal does not support
-        $databasePlatform = $schema->getDatabasePlatform();
-
-        foreach ($this->doctrineTypeMapping as $doctrineType => $dbTypes) {
-            foreach ($dbTypes as $dbType) {
-                $databasePlatform->registerDoctrineTypeMapping($dbType, $doctrineType);
-            }
-        }
-
-        $database = null;
-        if (strpos($table, '.')) {
-            list($database, $table) = explode('.', $table);
-        }
-
-        return $schema->listTableColumns($table, $database);
+        return \Schema::getColumns($table);
     }
 
     /**
